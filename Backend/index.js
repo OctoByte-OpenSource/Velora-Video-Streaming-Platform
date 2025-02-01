b2ae7ae77033837ed1250c895898f70f17a50e12
Rhishikesh Chaughule <rhishikeshchaughule@gmail.com>
2025-02-01 15:54:38 +0530

    fork request

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 5000;
+const port = 3000;
 
 // Define a route
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+	res.send("Hello rhishi!");
 });
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+	console.log(`Server running at http://localhost:${port}`);
 });
