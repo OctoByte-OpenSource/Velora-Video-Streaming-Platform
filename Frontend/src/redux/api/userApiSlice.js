711f8c3332108cdc71752eb882831b571c83dcd7
velora <forkthisrepo@gmail.com>
2025-02-15 19:57:56 +0530

    socket, query slice, auth and app layout

---
+++ Frontend/src/redux/api/userApiSlice.js
@@ -0,0 +1,33 @@
+import api from "./apiSlice";
+
+const userSlice = api.injectEndpoints({
+  endpoints: (builder) => ({
+    getUserInfo: builder.query({
+      query: (id) => ({
+        url: `user/info/${id}`,
+      }),
+    }),
+    getAllUsersInfo: builder.query({
+      query: (value) => ({
+        url: `user/getAllUsers?name=${value}`,
+      }),
+    }),
+    subscribeChannel: builder.mutation({
+      query: (id) => ({
+        url: `user/subscribeChannel/${id}`,
+        method: "POST",
+      }),
+    }),
+    // query for:
+    //-update profile
+    // - update bio
+  }),
+});
+
+export default userSlice;
+
+export const {
+  useLazyGetAllUsersInfoQuery,
+  useGetUserInfoQuery,
+  useSubscribeChannelMutation,
+} = userSlice;
