3e5964d08d676e2f53928c857d8f9dfdbce57e80
Rhishikesh Chaughule <rhishikeshchaughule@gmail.com>
2025-02-15 10:15:34 +0530

    fixed dashboard glitch

--- Frontend/src/AppComponents/Layout/appLayout.jsx
+++ Frontend/src/AppComponents/Layout/appLayout.jsx
@@ -3,11 +3,11 @@ import { Sidebar, Navbar } from "../../AppComponents/Navigation";
 
 const appLayout = () => {
 	return (
-		<div className='flex h-screen bg-white'>
+		<div className='flex h-screen overflow-y-scroll bg-white'>
 			<Sidebar />
 			<div className='flex-1 flex flex-col'>
 				<Navbar />
-				<div className='flex-1 p-1'>
+				<div className='flex-1 h-screen p-1'>
 					<DashBoard />
 				</div>
 			</div>
--- Frontend/src/pages/DashBoard.jsx
+++ Frontend/src/pages/DashBoard.jsx
@@ -4,33 +4,17 @@ import { data } from "@/data/data";
 
 const DashBoard = () => {
 	return (
-		<div>
-			<div className='grid grid-cols-4 gap-10 pl-2 pr-10 pt-2 pb-2'>
-				{data?.map((video) => (
-					// console.log("Hello", video.thumbnail)
-					<Card
-						key={video.id}
-						thumbnail={video.thumbnail}
-						description={video.description}
-						channel_name={video.channel_name}></Card>
-				))}
-			</div>
+		<div className='grid h-screen grid-cols-4 gap-10 pl-2 pr-10 pt-2 pb-2'>
+			{data?.map((video) => (
+				// console.log("Hello", video.thumbnail)
+				<Card
+					key={video.id}
+					thumbnail={video.thumbnail}
+					description={video.description}
+					channel_name={video.channel_name}></Card>
+			))}
 		</div>
 	);
 };
-
-// function DashBoard() {
-// 	return (
-// 		<div className='grid grid-cols-4 gap-4'>
-// 			{data?.map((video) => (
-// 				<div key={video.id} className='border p-4'>
-// 					<img src={video.thumbnail} alt={video.title} />
-// 					<h2 className='font-bold'>{video.title}</h2>
-// 					<p>{video.description}</p>
-// 				</div>
-// 			))}
-// 		</div>
-// 	);
-// }
 
 export default DashBoard;
