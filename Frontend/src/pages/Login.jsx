69b781bbbe23f14fb5c49cc6cfbae49509073a2e
SibinVarghese45 <sibinsunilvarghese@gmail.com>
2025-02-15 13:02:07 +0530

    Updated UI for login sign up

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,8 +1,6 @@
-import { Label } from "@/components/ui/label"
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
 import { useState } from "react"
 import { loginUser } from "@/api/auth"
+import { NavLink } from "react-router-dom"
 
 const Login = () => {
 
@@ -26,17 +24,48 @@ const Login = () => {
   }
 
   return (
-    <div className="flex justify-center items-center">
-        <form onSubmit={handleSubmit} className="w-[40%] mt-10 flex flex-col justify-center items-center">
-            <Label>Email: </Label>
-            < Input className="mt-2" type="email" placeholder = "Enter Email: " name="email" value={loginUserData.email} onChange={handleChange}></Input><br/><br/>
-            
-            <Label>Password: </Label>
-            < Input className="mt-2" type="password" placeholder = "Enter Password: " name="password" value={loginUserData.password} onChange={handleChange}></Input><br/><br/>
 
-            <Button>Submit</Button>
-        </form>
-    </div>
+    <section className="bg-gradient-to-r from-cyan-400 to-indigo-600">
+  <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+
+      <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-blue-500 md:text-2xl dark:text-white">
+                  Sign in to your account
+              </h1>
+              <form onSubmit={handleSubmit} className="space-y-4 md:space-y-6">
+                  <div>
+                      <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                      <input 
+                      name="email" value={loginUserData.email} onChange={handleChange}
+                      type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""/>
+                  </div>
+                  <div>
+                      <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                      <input
+                      value={loginUserData.password} onChange={handleChange}
+                      type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
+                  </div>
+                  <div className="flex items-center justify-between">
+                      <div className="flex items-start">
+                          <div className="flex items-center h-5">
+                            <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required=""/>
+                          </div>
+                          <div className="ml-3 text-sm">
+                            <label className="text-gray-500 dark:text-gray-300">Remember me</label>
+                          </div>
+                      </div>
+                      <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
+                  </div>
+                  <button type="submit" className="bg-blue-500 w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+                  <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                      Don’t have an account yet? <NavLink to='/register'>Sign Up</NavLink>
+                  </p>
+              </form>
+          </div>
+      </div>
+  </div>
+</section>
   )
 }
 
--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -1,8 +1,6 @@
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { Button } from "@/components/ui/button"
 import { useState } from "react"
 import { registerUser } from "@/api/auth"
+import { NavLink } from "react-router-dom"
 
 
 function Register() {
@@ -42,28 +40,66 @@ function Register() {
   }
 
   return (
-    <div className="flex justify-center items-center">
     
-          <form onSubmit={handleSubmit} className="w-[40%] mt-10 flex flex-col justify-start items-start gap-2">
-            <Label className = "text-white" >Username: </Label>
-            < Input className="mt-2 text-white" type="text" name="userName" value={formData.userName} placeholder = "Enter UserName: " onChange = {handleChange}></Input><br/><br/>
+    <section className="bg-gradient-to-r from-cyan-400 to-indigo-600">
+  <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
 
-            <Label className="text-white">Email: </Label>
-            < Input className="mt-2 text-white" type="email" name="email" value={formData.email} placeholder = "Enter Email: " onChange = {handleChange}></Input><br/><br/>
+      <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                  Create an account
+              </h1>
+              <form onSubmit={handleSubmit} className="space-y-4 md:space-y-6">
+                  <div>
+                      <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your UserName</label>
+                      <input
+                      name="userName" value={formData.userName} placeholder = "Enter UserName: " onChange = {handleChange}
+                      type="email" id="userName" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
+                  </div>
+                  <div>
+                      <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                      <input
+                      name="email" value={formData.email} placeholder = "Enter Email: " onChange = {handleChange}
+                      type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
+                  </div>
+                  <div>
+                      <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                      <input 
+                      name="password" value={formData.email} placeholder = "Enter Password: " onChange = {handleChange}
+                      type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
+                  </div>
+                  <div>
+                      <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
+                      <input
+                      name="confirmPassword" value={formData.confirmPassword} placeholder = "Confirm Password: " onChange = {handleChange}
+                      type="confirm-password" id="confirm-password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
+                  </div>
 
-            <Label className="text-white">Password: </Label>
-            < Input className="mt-2 text-white" type="password" name="password" value={formData.password} placeholder = "Enter Password: " onChange = {handleChange}></Input><br/><br/>
+                  <div>
+                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Upload Profile Picture</label>
+                        <input 
+                        onChange={handleFileChange}
+                        className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file"/>
+                  </div>
 
-            <Label className="text-white">Confirm Password: </Label>
-            < Input className="mt-2 text-white" type="text" name="confirmPassword" value={formData.confirmPassword} placeholder = "Confirm Password: " onChange = {handleChange}></Input><br/><br/>
-
-            <Label className="text-white">Profile Image: </Label>
-            < Input className="mt-2 text-white" type="file" onChange={handleFileChange}></Input><br/><br/>
-
-            <Button>Submit</Button>
-          </form>
-          
-    </div>
+                  <div className="flex items-start">
+                      <div className="flex items-center h-5">
+                        <input id="terms" aria-describedby="terms" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required=""/>
+                      </div>
+                      <div className="ml-3 text-sm">
+                        <label className="font-light text-gray-500 dark:text-gray-300">I accept the <a className="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>
+                      </div>
+                  </div>
+                  <button type="submit" className="bg-blue-500 w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
+                  <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                      Already have an account? <NavLink to='/login'>Login here</NavLink>
+                  </p>
+              </form>
+          </div>
+      </div>
+  </div>
+</section>
+      
 
     
   )
